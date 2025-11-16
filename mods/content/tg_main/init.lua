72c165be32e8976403f54644aec86db8191a2ac4
Sumi <info@sumianvoice.com>
2025-11-16 11:17:32 +1100

    decide skip intro based on mapgen type (flat), clarify `tg_main` flags `mg_name`, `dev_mode`

--- mods/content/tg_main/init.lua
+++ mods/content/tg_main/init.lua
@@ -1,14 +1,15 @@
 local mod_name = core.get_current_modname()
 local mod_path = core.get_modpath(mod_name)
 
-tg_main = {
-	skip_intro = true, -- skip the intro/welcome.. I need to work!!
-	mg_name = nil,
-  dev_mode = true,
-}
+tg_main = {}
 
+-- Either "flat" or "singlenode".
 tg_main.mg_name = core.get_mapgen_setting("mg_name") or "singlenode"
+-- Enter dev mode if mapgen "flat" or creative setting is `true`.
+-- This stops normal gameplay functions from running.
 tg_main.dev_mode = (tg_main.mg_name == "flat") or core.is_creative_enabled()
+-- Skip intro if on mapgen "flat".
+tg_main.skip_intro = (tg_main.mg_name == "flat")
 
 dofile(mod_path .. "/scripts" .. "/math.lua")
 dofile(mod_path .. "/scripts" .. "/debug.lua")
