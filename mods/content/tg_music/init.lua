ccabcb2fc04c6330ac9135ef11ffb176a3f19766
KINGTHEGUY <kingtheguy@proton.me>
2025-12-04 19:55:51 -0500

    some sounds related stuff

--- mods/content/tg_music/init.lua
+++ mods/content/tg_music/init.lua
@@ -101,4 +101,26 @@ core.register_on_joinplayer(function(player)
         plr = player,
         name = player:get_player_name()
     }
+    core.sound_play({
+        name = "paradise_sightseer",
+        gain = 0.2,
+        to_player = player:get_player_name(),
+        loop = true,
+    })
+
+    --spec simplesoundspec
+    --params
+
+    --core.sound_stop
+    core.sound_play({
+        name = "paradise_sightseer",
+        gain = 0.1,
+        to_player = player:get_player_name(),
+        loop = true,
+    })
+
+    --spec simplesoundspec
+    --params
+
+    --core.sound_stop
 end)
