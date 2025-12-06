43c7ee1dafbc79783dc2609b0e202ed8abf1e160
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2025-12-06 17:13:22 -0500

    remove aom wrench, not needed in base game, remove pointless modpacks

--- mods/content/aom_wrench/init.lua
+++
@@ -1,8 +0,0 @@
-local mod_name = core.get_current_modname()
-local mod_path = core.get_modpath(mod_name)
-
-aom_wrench = {}
-
-dofile(mod_path .. "/scripts/system.lua")
-dofile(mod_path .. "/items/wrench.lua")
-dofile(mod_path .. "/compat.lua")
