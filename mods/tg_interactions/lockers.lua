5530a6c08161da2eedf2e70188bbd3e26cf3b792
TPH <DamotrixRob@gmail.com>
2026-01-23 22:55:08 +0000

    tg_interactions: utilize new `tg_time.register_timed_from_worldcreate` for locker generation, save `locker_function_id` and `locker_node` for locker entities

--- mods/tg_interactions/lockers.lua
+++ mods/tg_interactions/lockers.lua
@@ -52,86 +52,85 @@ end
 local errtext = "tg_interacts lockers: had an issue with generating an area."
 
 -- run functionality for locker loot areas
-core.register_on_newplayer(function(plr)
-    core.after(4, function()
-        for _,area in ipairs(areas) do
-            local poss = area.positions
-            -- looks for "tg_nodes:locker" between start and end, grouped `true` returns list
-            -- of positions associated to node name
-            local nodesraw = core.find_nodes_in_area(poss.start, poss.finish,
-              {"tg_nodes:locker"}, true)
-            -- ensure always a table
-            nodesraw = nodesraw["tg_nodes:locker"] or {}
-            -- get and check length of found nodes
-            local len = #nodesraw
-            if len == 0 then 
-                error(errtext.." Could not find any 'tg_nodes:locker' nodes in area between ( "..
-                  core.pos_to_string(poss.start).." - "..core.pos_to_string(poss.finish)..
-                  " ). You should check if there are locker nodes in this area before registering a locker area"..
-                  ". Error with mod '"..area.mod_origin.."'")
-            end
-            -- refined nodes
-            local nodes = {}
-            -- add additional information to each detected node
-            for ind,pos in ipairs(nodesraw) do -- index, position
-                local node = core.get_node(pos)
-                -- shouldn't be unknown, but we'll add it anyways
-                local facing = (
-                    node.param2 == 3 and "+X" or node.param2 == 1 and "-X" or
-                    node.param2 == 2 and "+Z" or node.param2 == 0 and "-Z" or "unknown"
-                )
-                -- entity position (for indicator)
-                -- ensure infront of locker (use param2)
-                local epos = pos:add(
-                  vector.new(
-                    -- X
-                    facing=="+X" and 0.6 or facing=="-X" and -0.6 or 0,
-                    -- Y
-                    1,
-                    -- Z
-                    facing=="+Z" and 0.6 or facing=="-Z" and -0.6 or 0
-                  )
-                )
-                -- check for conflicts
-                -- create a checkpos due to annoying rounding
-                local checkpos = epos:add(
-                  vector.new(
-                    -- X
-                    facing=="+X" and 0.4 or facing=="-X" and -0.4 or 0,
-                    -- Y (subtract 1 from entity)
-                    -1,
-                    -- Z
-                    facing=="+Z" and 0.4 or facing=="-Z" and -0.4 or 0
-                  )
-                )
-                local conflict = core.get_node(checkpos)
-                -- ok, bottom area is ALRIGHT (not a locker)
-                if conflict.name ~= "tg_nodes:locker" then
-                    -- check above
-                    checkpos.y = checkpos.y + 1
-                    conflict = core.get_node(epos)
-                    conflict = core.registered_nodes[conflict.name] or {name="ignore"}
-                    -- can place an entity here!
-                    if conflict.drawtype == "airlike" or conflict.name == "ignore" then
-                        -- add to refined
-                        table.insert(nodes, {pos = pos, node = node, epos = epos, face=facing})
-                    end
+tg_time.register_timed_from_worldcreate(4, function(time)
+    core.log("GEN LOCKERS "..time)
+    for _,area in ipairs(areas) do
+        local poss = area.positions
+        -- looks for "tg_nodes:locker" between start and end, grouped `true` returns list
+        -- of positions associated to node name
+        local nodesraw = core.find_nodes_in_area(poss.start, poss.finish,
+          {"tg_nodes:locker"}, true)
+        -- ensure always a table
+        nodesraw = nodesraw["tg_nodes:locker"] or {}
+        -- get and check length of found nodes
+        local len = #nodesraw
+        if len == 0 then
+            error(errtext.." Could not find any 'tg_nodes:locker' nodes in area between ( "..
+              core.pos_to_string(poss.start).." - "..core.pos_to_string(poss.finish)..
+              " ). You should check if there are locker nodes in this area before registering a locker area"..
+              ". Error with mod '"..area.mod_origin.."'")
+        end
+        -- refined nodes
+        local nodes = {}
+        -- add additional information to each detected node
+        for ind,pos in ipairs(nodesraw) do -- index, position
+            local node = core.get_node(pos)
+            -- shouldn't be unknown, but we'll add it anyways
+            local facing = (
+                node.param2 == 3 and "+X" or node.param2 == 1 and "-X" or
+                node.param2 == 2 and "+Z" or node.param2 == 0 and "-Z" or "unknown"
+            )
+            -- entity position (for indicator)
+            -- ensure infront of locker (use param2)
+            local epos = pos:add(
+              vector.new(
+                -- X
+                facing=="+X" and 0.6 or facing=="-X" and -0.6 or 0,
+                -- Y
+                1,
+                -- Z
+                facing=="+Z" and 0.6 or facing=="-Z" and -0.6 or 0
+              )
+            )
+            -- check for conflicts
+            -- create a checkpos due to annoying rounding
+            local checkpos = epos:add(
+              vector.new(
+                -- X
+                facing=="+X" and 0.4 or facing=="-X" and -0.4 or 0,
+                -- Y (subtract 1 from entity)
+                -1,
+                -- Z
+                facing=="+Z" and 0.4 or facing=="-Z" and -0.4 or 0
+              )
+            )
+            local conflict = core.get_node(checkpos)
+            -- ok, bottom area is ALRIGHT (not a locker)
+            if conflict.name ~= "tg_nodes:locker" then
+                -- check above
+                checkpos.y = checkpos.y + 1
+                conflict = core.get_node(epos)
+                conflict = core.registered_nodes[conflict.name] or {name="ignore"}
+                -- can place an entity here!
+                if conflict.drawtype == "airlike" or conflict.name == "ignore" then
+                    -- add to refined
+                    table.insert(nodes, {pos = pos, node = node, epos = epos, face=facing})
                 end
             end
-            -- check length again after filtering
-            len = #nodes
-            if len == 0 then
-                error(errtext.." Could not find valid lockers in area between ( "..
-                  core.pos_to_string(poss.start).." - "..core.pos_to_string(poss.finish)..
-                  " ). Please check that the node's param2 is correctly rotated and that there is not a non-air "..
-                  "node blocking, so that entity indicators can be appropriately placed. "..
-                  "Error with mod '"..area.mod_origin.."'")
-            end
-            -- run generate function
-            -- table of positions, "bounds", total number of found lockers
-            area.generate(nodes, area.positions, len)
         end
-    end)
+        -- check length again after filtering
+        len = #nodes
+        if len == 0 then
+            error(errtext.." Could not find valid lockers in area between ( "..
+              core.pos_to_string(poss.start).." - "..core.pos_to_string(poss.finish)..
+              " ). Please check that the node's param2 is correctly rotated and that there is not a non-air "..
+              "node blocking, so that entity indicators can be appropriately placed. "..
+              "Error with mod '"..area.mod_origin.."'")
+        end
+        -- run generate function
+        -- table of positions, "bounds", total number of found lockers
+        area.generate(nodes, area.positions, len)
+    end
 end)
 
 
@@ -226,7 +225,23 @@ tg_interactions.register_interactable("locker_interactable", "none", "", "tg_nod
     end,
     -- interacting
     on_rightclick = locker_interact,
-    on_punch = locker_interact
+    on_punch = locker_interact,
+    -- loading
+    on_activate = function(self, staticdata, dtime_s)
+        if staticdata == "" then return end -- nothing
+        local data = core.deserialize(staticdata)
+        -- load into self
+        for name, value in pairs(data) do
+            self[name] = value
+        end
+    end,
+    -- saving
+    get_staticdata = function(self)
+        return core.serialize({
+            locker_function_id = self.locker_function_id,
+            locker_node = self.locker_node
+        })
+    end,
 })
 
 
