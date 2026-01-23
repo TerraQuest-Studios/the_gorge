f52ca8762d8c401ff24e8333e8e18e65c6f842af
TPH <DamotrixRob@gmail.com>
2026-01-23 21:45:51 +0000

    tg_time: add `tg_time.register_timed_from_worldcreate` function for handling running one-off functions based off of when the world was created (to prevent duplication or inability to spawn stuff)

--- mods/tg_time/init.lua
+++ mods/tg_time/init.lua
@@ -6,6 +6,9 @@ provides functionality for on world start as well!
 
 --]]
 
+-- create global
+tg_time = {}
+
 -- active time, starts ticking upon world globalstep initiation
 local activetime = 0
 
@@ -19,49 +22,65 @@ local function store_gametime()
     modstor:set_float("gametime", gametime)
 end
 
--- for `core.after` like functionality
--- adds each dependent function to here
-local funclist = {}
-
--- store each function into here :shrug:
-tg_time = {
-    -- permits "since" argument for getting `activetime - since`
-    --- provides `activetime`, or a number subtracted by activetime if provided a `since`
-    ---@param since? number if provided, subtracts activetime by said number
-    ---@return number
-    get_time = function(since)
-        return type(since) == "number" and activetime - since or activetime
-    end,
-    
-    --- provides `gametime`, or a number subtracted by gametime if provided a `since`
-    ---@param since? number if provided, subtracts gaetime by said number
-    ---@return number
-    get_overall_time = function(since)
-        -- may as well store it here too
-        store_gametime()
-        return type(since) == "number" and gametime - since or gametime
-    end,
-
-    -- `core.after` function
-    --- similar to `core.after`, expect provides the amount of time it took for function to be ran as first argument
-    after = function(delay, func, ...)
-        if not tonumber(delay) or core.is_nan(delay) then
-            error(
-                "tg_time.after: invalid invocation - provided delay argument is not number or is an impossible number"
-            )
-        end
-        if type(func) ~= "function" then
-            error("tg_time.after: invalid invocation - provided func argument is not a function. Got "..type(func))
-        end
-        -- add to list
-        funclist[#funclist + 1] = {
-            delay = delay,
-            func = func,
-            start = activetime,
-            args = {...}
-        }
-    end
+-- unique events
+local funclist = {
+    -- for `core.after` like functionality
+    -- adds each dependent function to here
+    after = {},
+    -- for functionality of running a ONE-TIME function after a set time has elapsed
+    -- since the world was created (e.g. spawning important entities, stuff to do on world creation)
+    worldcreate = {},
 }
+
+-- start from when world was created
+function tg_time.register_timed_from_worldcreate(time, func)
+    if time < tg_time.get_overall_time() then return end -- no need, the time has already passed
+    -- add to list
+    table.insert(funclist.worldcreate, {
+        func = func,
+        time = time,
+    })
+end
+
+
+funclist.after = {}
+
+-- permits "since" argument for getting `activetime - since`
+--- provides `activetime`, or a number subtracted by activetime if provided a `since`
+---@param since? number if provided, subtracts activetime by said number
+---@return number
+function tg_time.get_time(since)
+    return type(since) == "number" and activetime - since or activetime
+end
+    
+--- provides `gametime`, or a number subtracted by gametime if provided a `since`
+---@param since? number if provided, subtracts gaetime by said number
+---@return number
+function tg_time.get_overall_time(since)
+    -- may as well store it here too
+    store_gametime()
+    return type(since) == "number" and gametime - since or gametime
+end
+
+-- `core.after` function
+--- similar to `core.after`, expect provides the amount of time it took for function to be ran as first argument
+function tg_time.after(delay, func, ...)
+    if not tonumber(delay) or core.is_nan(delay) then
+        error(
+            "tg_time.after: invalid invocation - provided delay argument is not number or is an impossible number"
+        )
+    end
+    if type(func) ~= "function" then
+        error("tg_time.after: invalid invocation - provided func argument is not a function. Got "..type(func))
+    end
+    -- add to list
+    table.insert(funclist.after, {
+        delay = delay,
+        func = func,
+        start = activetime,
+        args = {...}
+    })
+end
 
 -- save gametime check
 local SGC = 120 -- save every 2 minutes
@@ -76,8 +95,17 @@ core.register_globalstep(function(dtime)
         store_gametime()
         SGC = 120
     end
+    -- run worldcreate stuff
+    for ind,data in ipairs(funclist.worldcreate) do
+        if gametime > data.time then
+            -- provide time elapsed as argument
+            data.func(gametime)
+            -- remove
+            table.remove(funclist.worldcreate, ind)
+        end
+    end
     -- run `core.after` esque stuff
-    for ind,data in ipairs(funclist) do
+    for ind,data in ipairs(funclist.after) do
         local elapsed = tg_time.get_time(data.start)
         -- has been past the delay!
         if elapsed > data.delay then
