d636d02cb38333ae6f4ca71d21cbba10f8312359
TPH <damotrixrob@gmail.com>
2026-01-28 13:36:53 +0000

    tg_player: event callbacks for (successfully) crouching and proning, audio for proning and prone movement (disabling of making node footstep sounds in addition if proning), convert `eye_height` player variables into table of (stand, sneak, prone), function for getting player's data and function for changing player's `eye_height` and collision box upper Y coordinate, prevent getting up from crouching/proning if underneath a node (#22)
    * tg_player: add event callbacks for (successfully) crouching, proning, and getting back up. Play sound for successful proning. Stop footstep sounds when proning
    
    * tg_player: add mechanics and audios for prone moving sounds
    
    * tg_player: table'ify `tg_player.eye_height` (stand, sneak, prone), add function for grabbing player's data. Add function for changing player's eye_height and its correlating collisionbox Y coordinate. Handle setting of player texture in other joinplayer function
    
    * tg_player: prevent player from getting up if underneath a solid node
    
    * tg_player: improve mechanics with being stuck underneath a node while crouching or proning (introduce `pdata.try_getting_up`)

--- mods/tg_player/init.lua
+++ mods/tg_player/init.lua
@@ -3,9 +3,11 @@ local mod_path = core.get_modpath(mod_name)
 
 tg_player = {}
 
-tg_player.eye_height = 1.625
-tg_player.eye_height_sneak = 1.3
-tg_player.eye_height_prone = 0.5
+tg_player.eye_height = {
+    stand = 1.625, -- up and about
+    sneak = 1.3, -- crouching
+    prone = 0.5 -- on the ground like a worm
+}
 
 dofile(mod_path .. "/scripts" .. "/helpers.lua")
 
@@ -38,9 +40,6 @@ core.register_on_joinplayer(function(player, last_login)
 	-- if tg_main.dev_mode == true then
 	-- else
 	-- end
-	local props = player:get_properties()
-	props.textures = { "player.png" }
-	player:set_properties(props)
 	player:set_lighting({
 		shadows = { intensity = 0.33 },
 		volumetric_light = { strength = 0.45 },
@@ -80,6 +79,10 @@ for _, ename in ipairs({ -- for _, event name
     "change_lookdir",
     -- either player's eye pos or lookdir changed
     "change_eyepos_or_lookdir",
+    -- additional player movements
+    "crouch_success",
+    "prone_success",
+    "gotup"
 }) do
     -- name, automatic setup definition: add register function to `tg_interactions`
     -- return will be data of this event
@@ -89,15 +92,58 @@ end
 
 -- array of data of players
 local pdatas = {}
+
+-- player data related functions
+
+--- function for returning data on select player
+--- @param plr userdata select player
+function tg_player.get_data(plr)
+    -- iterate over player datas to compare object
+    for ind,pdata in ipairs(pdatas) do
+        -- permit the return of an index (ind) as 2nd return
+        -- (index should NOT be used by external mods nor stored due to dynamicism)
+        if plr == pdata.obj then
+            return pdata, ind
+        end
+    end
+end
+
+--- function for changing player's eye height alongside their collision box
+--- @param plr userdata player to change eye_height of
+--- @param eye_height number
+--- @param pdata? table data on current player, does not need to be specified
+function tg_player.change_eye_height(plr, eye_height, pdata)
+    if type(eye_height) ~= "number" then return end -- not a number
+    pdata = pdata or tg_player.get_data(plr)
+    if not pdata then return end -- how??? what did you do???
+    pdata.props = pdata.props or plr:get_properties()
+    local props = pdata.props
+    -- now to change collisionbox
+    local colbox = pdata.props.collisionbox
+    -- colbox[2] SHOULD BE 0, but JUST in case, let's use it here!
+    -- add bit of our head to the eye_height as well (0.145)
+    colbox[5] = colbox[2] + (eye_height + 0.145)
+    -- now set changed eye height
+    props.eye_height = eye_height
+    -- set
+    plr:set_properties(pdata.props)
+end
+
+
 -- create data
 core.register_on_joinplayer(function(plr)
     local pdata = {
         obj = plr,
+        props = plr:get_properties(),
         name = plr:get_player_name(),
         time = 0, -- total time
         held_keys = {} -- data on each key pressed (time)
     }
     pdatas[#pdatas + 1] = pdata
+    -- change texture
+    pdata.props.textures = { "player.png" }
+    -- changes eye height + sets properties
+    tg_player.change_eye_height(plr, tg_player.eye_height.stand, pdata)
     -- event
     events.join(plr, pdata)
 end)
@@ -110,11 +156,10 @@ local function on_leave(plr, pdata, index)
 end
 -- player leaving
 core.register_on_leaveplayer(function(plr)
-    for ind,pdata in ipairs(pdatas) do
-        -- found! remove
-        if pdata.obj == plr then
-            return on_leave(plr, pdata, ind)
-        end
+    local pdata, ind = tg_player.get_data(plr)
+    -- found! remove
+    if pdata then
+        return on_leave(plr, pdata, ind)
     end
 end)
 -- server shutdown
@@ -257,26 +302,37 @@ end)
 events.keypress_step.register(function(plr, pdata, key, time)
     if key ~= "sneak" then return end -- not pressing sneak
     if pdata.held_keys.jump then return end -- JUMPING, AAAH!!!
+    if pdata.getting_up then return end -- can't crouch if getting up
     -- sneaky time
     local props = pdata.props -- player's properties
     local eheight = props.eye_height
     -- ideal eye height
-    local IEH = pdata.proning and tg_player.eye_height_prone or
-      tg_player.eye_height_sneak
+    local IEH = pdata.proning and tg_player.eye_height.prone or
+      tg_player.eye_height.sneak
     -- slowly transitioning down
-    if eheight ~= IEH then
+    -- value will not stay the same, check difference
+    if math.abs(eheight - IEH) > 0.001 then
         local dtimeperc = pdata.dtime/0.025 -- delta time percentage (every 25ms)
-        -- subtract by 0.06 per dtimeperc, clamp to IEH if below
-        props.eye_height = math.max(eheight - (0.06*dtimeperc), IEH)
-        -- update player properties
-        plr:set_properties(props)
+        -- subtract by 0.045 per dtimeperc, clamp to IEH if below
+        eheight = math.max(eheight - (0.045*dtimeperc), IEH)
+        -- update player eye_height + set properties
+        tg_player.change_eye_height(plr, eheight, pdata)
         -- gradually change proning player's look
         if pdata.proning then
             local clook = math.deg(plr:get_look_vertical()) -- current look
             if clook > 10 then
                 -- subtract by 6 per dtimeperc
-                clook = math.max(clook - (8*dtimeperc), 10) -- clamp above 10
+                clook = math.max(clook - (6*dtimeperc), 10) -- clamp above 10
                 plr:set_look_vertical(math.rad(clook))
+            end
+        end
+        -- check if equal now
+        if props.eye_height == IEH then
+            -- run callbacks
+            if pdata.proning then
+                events.prone_success(plr, pdata)
+            else
+                events.crouch_success(plr, pdata)
             end
         end
     end
@@ -307,40 +363,145 @@ end)
 events.keypress_end.register(function(plr, pdata, key, time)
     if key ~= "sneak" then return end -- not sneaking
     if pdata.getting_up then return end -- we're already getting up!
-    -- we're getting up!
-    pdata.getting_up = true
-    -- no longer sneaking or proning
-    pdata.sneaking = nil
-    pdata.proning = nil
+    -- we're gon try getting up!
+    pdata.try_getting_up = true
 end)
 
 -- ran each globalstep
 -- handle getting up from sneaking/proning
 events.step.register(function(plr, pdata)
+    if pdata.try_getting_up then
+        -- add 1.5 +Y to check node above
+          local anode = core.get_node(pdata.pos:add(vector.new(0, 1.5, 0) ) )
+          anode = core.registered_nodes[anode.name]
+          if anode and anode.walkable then return end -- can't crouch up from here
+          -- definitely no longer sneaking or proning
+          pdata.sneaking = nil
+          pdata.proning = nil
+          -- now getting up
+          pdata.getting_up = true
+          pdata.try_getting_up = nil -- we're already getting up now!
+    end
+    -- don't run rest of code til this is done
     if not pdata.getting_up then return end
     local props = pdata.props -- player properties
     local eheight = props.eye_height
     -- ideal eye height
-    local IEH = tg_player.eye_height
+    local IEH = tg_player.eye_height.stand
     -- transitioning back up
     if eheight ~= IEH then
         -- add by 0.15, clamp to IEH if above
-        props.eye_height = math.min(eheight + 0.15, IEH)
+        eheight = math.min(eheight + 0.15, IEH)
         -- update player properties
-        plr:set_properties(props)
+        tg_player.change_eye_height(plr, eheight, pdata)
     end
     -- completed!
-    if eheight == IEH then
+    if props.eye_height == IEH then
         pdata.getting_up = nil
+        events.gotup(plr, pdata)
     end
 end)
 
 -- ran each keypress step
 -- can't crouch if we're jumping!
 events.keypress_step.register(function(plr, pdata, key, time)
-    if key == "jump" then
-        pdata.sneaking = nil
-        pdata.proning = nil
-        pdata.getting_up = true
+    if key ~= "jump" then return end
+    -- only do stuff if proning or sneaking
+    if not (pdata.sneaking or pdata.proning) then return end
+    -- probably not healthy checking this each jump step, but eh
+    -- add 1.5 +Y to check node above
+    local anode = core.get_node(pdata.pos:add(vector.new(0, 1.5, 0) ) )
+    anode = core.registered_nodes[anode.name]
+    if anode and anode.walkable then return end -- can't crouch up from here
+    pdata.sneaking = nil
+    pdata.proning = nil
+    pdata.getting_up = true
+end)
+
+-- proning extras
+events.prone_success.register(function(plr, pdata)
+    -- hitting floor sound
+    core.sound_play("tg_player_prone", {
+        obj = plr,
+        pitch = math.random(95,120)/100
+    })
+    -- remove footstep sound
+    local props = pdata.props
+    if not props then return end
+    props.makes_footstep_sound = false
+    plr:set_properties(props)
+end)
+
+-- fix footstep sound
+events.gotup.register(function(plr, pdata)
+    local props = pdata.props
+    if not props then return end
+    if not props.makes_footstep_sound then
+        props.makes_footstep_sound = true
+        plr:set_properties(props)
+    end
+end)
+
+-- prone movement sounds
+-- create pronesound
+events.prone_success.register(function(plr, pdata)
+    pdata.prone_sound = {poscheck = 0} -- start at 0 to prevent sound until moving
+end)
+
+-- update poscheck
+events.change_pos.register(function(plr, pdata, pos, oldpos)
+    local pronesound = pdata.prone_sound
+    if not pronesound then return end -- no point!
+    if pos.y ~= oldpos.y then return end -- falling or jumping!
+    -- reset pos check
+    pronesound.poscheck = 0.2
+end)
+
+-- play or stop proning sounds
+events.step.register(function(plr, pdata)
+    local pronesound = pdata.prone_sound
+    local sound = pronesound and pronesound.id -- return of `core.sound_play`
+    -- no longer proning
+    if not pdata.proning then
+        if sound then
+            core.sound_fade(sound, 2, 0)
+            pdata.prone_sound = nil -- erase
+        end
+        return
+    end
+    if not pronesound then return end -- can't do anything, man!
+    -- don't play any sound if poscheck is 0
+    if pronesound.poscheck == 0 then
+        if sound then
+            core.sound_fade(sound, 0.4, 0)
+            --pdata.prone_sound = nil -- erase
+        end
+        return
+    end
+    -- get dtime, total time, length of sound
+    local dtime = pdata.dtime or 0
+    local ttime = pdata.time
+    local len = pdata.playlength or 0.8
+    -- and count down
+    -- clamp above 0
+    pronesound.poscheck = math.max(pronesound.poscheck - dtime, 0)
+    -- check if should play sound again
+    local lastplayed = pronesound.playedat
+    -- no playedat means haven't played, or time since is greater than playlength
+    if not lastplayed or (ttime - lastplayed) > len then
+        if sound then
+            core.sound_fade(sound, 2, 0)
+        end
+        local pitch = math.random(60, 110)/100
+        pronesound.id = core.sound_play("tg_player_prone_move", {
+            --obj = plr,
+            pos = pdata.pos,
+            pitch = pitch,
+            gain = math.random(5,10)/100
+        })
+        -- set timing
+        pronesound.playedat = ttime
+        -- expected length divided by pitch
+        pronesound.playlength = 1.6/pitch
     end
 end)
